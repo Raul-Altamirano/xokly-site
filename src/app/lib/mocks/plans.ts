ca0c6828703ee6c2bb74a481abc0c69552c63f3d
Raul Conrado Sanchez Altamirano <altamirano262010@hotmail.com>
2026-04-09 21:37:36 -0600

    fix formatPrice mxn

--- src/app/lib/mocks/plans.ts
+++ src/app/lib/mocks/plans.ts
@@ -57,5 +57,5 @@ export const plans = [
 
 export function formatPrice(plan: typeof plans[0]) {
     if (plan.price === null) return "Próximamente";
-    return `$${(plan.price as number).toLocaleString("es-MX")} ${plan.currency}/${plan.period}`;
+return `$${new Intl.NumberFormat("es-MX").format(plan.price)} ${plan.currency}/${plan.period}`;
 }
